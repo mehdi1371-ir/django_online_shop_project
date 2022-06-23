aadc1d7776560a3c3ee321cb62e71190cacd9177
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-06-23 19:37:52 +0430

    CustomUser model created

---
+++ accounts/admin.py
@@ -0,0 +1,16 @@
+from atexit import register
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email', 'username')
+
+
