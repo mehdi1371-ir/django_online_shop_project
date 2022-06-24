8d2cf5120d94e96fa4bdea7f99159da7de2ed29b
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-06-24 17:29:51 +0430

    add homepage and aboutus tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PagesTest(TestCase):
+
+    def test_home_page_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name= 'home.html')
+
+    def test_about_page_view(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name= 'pages/about.html')
