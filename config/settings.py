ff4a867dd86ea5b65717247789bad9c542c631de
mehdi pourbaba <mhdpourbaba@gmail.com>
2022-07-22 15:04:37 +0430

    add ProductListView and ProductDetailView

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 # For environment variables
 env = Env()
@@ -144,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
